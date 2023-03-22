be0f957a611206b96021d40a662f5a841bf24195
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-22 22:07:26 +0700

    Написал код к задаче №36

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿void CreatingArray (int[] array) {
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+int Sum (int[] array) {
+    int sum = 0;
+        for (int i = 1; i < array.Length; i += 2){
+            sum += array[i];
+        }
+        return sum;
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: >> ");
+int num = int.Parse (Console.ReadLine()!);
+int[] array = new int[num];
+CreatingArray(array);
+Sum(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {Sum(array)}");
+Console.WriteLine();
