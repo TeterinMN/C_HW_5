f607c2b70b233916dcd07afd180f196cd8593bc0
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-23 00:14:59 +0700

    Закончил код для доп. задания

--- Task_Dop/Program.cs
+++ Task_Dop/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿Console.Clear();
+int[] a = new int[100];
+int[] b = new int[100];
+int[] c = new int[100];
+int n = 0, k = 0, d = 0;
+
+Console.Write("Введите количество дней месяца в которые Вася получал оценки: >> ");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число месяца: >> ");
+    a[i] = int.Parse(Console.ReadLine());
+    if (a[i] % 2 == 1)
+    {
+        b[i] = a[i];
+        k++;
+    }
+    else
+    {
+        c[i] = a[i];
+        d++;
+    }
+}
+
+Console.WriteLine();
+Console.Write("В эти дни Вася получал тройки: ");
+for (int i = 0; i < n; i++)
+{
+    if (b[i] != 0)
+        Console.Write(b[i] + " ");
+}
+
+Console.WriteLine();
+Console.Write("В эти дни Вася получал четверки: ");
+for (int i = 0; i < n; i++)
+{
+    if (c[i] != 0)
+        Console.Write(c[i] + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine("Ответ: ");
+if (k > d)
+    Console.WriteLine("NO - Нет, так как троек больше чем четверок");
+else
+    Console.WriteLine("YES - Да, Вася остается ударником :-)");
